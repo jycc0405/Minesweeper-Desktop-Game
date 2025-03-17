99585f1cf6191ab02949f3df0ba961cebfb06c63
jycc0405 <121859759+jycc0405@users.noreply.github.com>
2025-03-17 17:05:03 +0900

    Update Board.java
    오류 수정

--- Code/src/minesweeper/Board.java
+++ Code/src/minesweeper/Board.java
@@ -1,36 +1,29 @@
 package minesweeper;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import javafx.util.Pair;
 
+import java.sql.*;
 
 
-public class Board 
-{
-    private int numberOfMines;	
+public class Board {
+    private int numberOfMines;
     private Cell cells[][];
 
     private int rows;
     private int cols;
 
-        
+
     //---------------------------------------------//
-    
-    public Board(int numberOfMines, int r, int c)
-    {
+
+    public Board(int numberOfMines, int c, int r) {
         this.rows = r;
         this.cols = c;
         this.numberOfMines = numberOfMines;
 
-        cells = new Cell[rows][cols];
+        cells = new Cell[cols][rows];
 
         //Step 1: First create a board with empty Cells
-        createEmptyCells();         
+        createEmptyCells();
 
         //Step 2: Then set mines randomly at cells
         setMines();
@@ -42,12 +35,9 @@ public class Board
 
     //------------------------------------------------------------------//
     //STEP 1//
-    public void createEmptyCells()
-    {
-        for (int x = 0; x < rows; x++)
-        {
-            for (int y = 0; y < cols; y++)
-            {
+    public void createEmptyCells() {
+        for (int x = 0; x < cols; x++) {
+            for (int y = 0; y < rows; y++) {
                 cells[x][y] = new Cell();
             }
         }
@@ -55,59 +45,50 @@ public class Board
 
     //------------------------------------------------------------------//
     //STEP 2//
-    public void setMines()
-    {
-        int x,y;
+    public void setMines() {
+        int x, y;
         boolean hasMine;
-        int currentMines = 0;                
+        int currentMines = 0;
 
-        while (currentMines != numberOfMines)
-        {
+        while (currentMines != numberOfMines) {
             // Generate a random x coordinate (between 0 and cols)
-            x = (int)Math.floor(Math.random() * rows);
+            x = (int) Math.floor(Math.random() * cols);
 
             // Generate a random y coordinate (between 0 and rows)
-            y = (int)Math.floor(Math.random() * cols);
+            y = (int) Math.floor(Math.random() * rows);
 
             hasMine = cells[x][y].getMine();
 
-            if(!hasMine)
-            {		
+            if (!hasMine) {
                 cells[x][y].setMine(true);
-                currentMines++;	
-            }			
+                currentMines++;
+            }
         }
     }
     //------------------------------------------------------------------//
 
     //------------------------------------------------------------------//
     //STEP 3//
-    public void setSurroundingMinesNumber()
-    {	
-        for(int x = 0 ; x < rows ; x++)
-        {
-            for(int y = 0 ; y < cols ; y++)
-            {
-                cells[x][y].setSurroundingMines(calculateNeighbours(x,y));                        
+    public void setSurroundingMinesNumber() {
+        for (int x = 0; x < cols; x++) {
+            for (int y = 0; y < rows; y++) {
+                cells[x][y].setSurroundingMines(calculateNeighbours(x, y));
             }
         }
     }
     //------------------------------------------------------------------//	
 
     //------------------------------------------------------------------//
-    public int getSurroundingFlagNumber(int xCo,int yCo)
-    {
-        int flagNumber=0;
+    public int getSurroundingFlagNumber(int xCo, int yCo) {
+        int flagNumber = 0;
 
         // Check the neighbours (the columns xCo - 1, xCo, xCo + 1)
-        for(int x=makeValidCoordinateX(xCo - 1); x<=makeValidCoordinateX(xCo + 1); x++)
-        {
+        for (int x = makeValidCoordinateX(xCo - 1); x <= makeValidCoordinateX(xCo + 1); x++) {
             // Check the neighbours (the rows yCo - 1, yCo, yCo + 1).
-            for(int y=makeValidCoordinateY(yCo - 1); y<=makeValidCoordinateY(yCo + 1); y++)
-            {
+            for (int y = makeValidCoordinateY(yCo - 1); y <= makeValidCoordinateY(yCo + 1); y++) {
                 // Skip (xCo, yCo), since that's no neighbour.
-                if(x != xCo || y != yCo)
-                    if(cells[x][y].getContent().equals("F"))   // If the neighbour contains a mine, neighbours++.
+                if (x != xCo || y != yCo)
+                    if (cells[x][y].getContent().equals("F"))   // If the neighbour contains a mine, neighbours++.
                         flagNumber++;
             }
         }
@@ -116,26 +97,22 @@ public class Board
     }
 
 
-
     //------------------------------------------------------------------//
 
 
     //---------------------HELPER FUNCTIONS---------------------------//        
 
     //Calculates the number of surrounding mines ("neighbours")
-    public int calculateNeighbours(int xCo, int yCo)
-    {
+    public int calculateNeighbours(int xCo, int yCo) {
         int neighbours = 0;
 
         // Check the neighbours (the columns xCo - 1, xCo, xCo + 1)
-        for(int x=makeValidCoordinateX(xCo - 1); x<=makeValidCoordinateX(xCo + 1); x++) 
-        {
+        for (int x = makeValidCoordinateX(xCo - 1); x <= makeValidCoordinateX(xCo + 1); x++) {
             // Check the neighbours (the rows yCo - 1, yCo, yCo + 1).
-            for(int y=makeValidCoordinateY(yCo - 1); y<=makeValidCoordinateY(yCo + 1); y++) 
-            {
+            for (int y = makeValidCoordinateY(yCo - 1); y <= makeValidCoordinateY(yCo + 1); y++) {
                 // Skip (xCo, yCo), since that's no neighbour.
-                if(x != xCo || y != yCo)
-                    if(cells[x][y].getMine())   // If the neighbour contains a mine, neighbours++.
+                if (x != xCo || y != yCo)
+                    if (cells[x][y].getMine())   // If the neighbour contains a mine, neighbours++.
                         neighbours++;
             }
         }
@@ -146,98 +123,89 @@ public class Board
     //------------------------------------------------------------------//	
 
     //Simply makes a coordinate a valid one (i.e within the boundaries of the Board)
-    public int makeValidCoordinateX(int i)
-    {
+    public int makeValidCoordinateX(int i) {
         if (i < 0)
             i = 0;
-        else if (i > rows-1)
-            i = rows-1;
+        else if (i > cols - 1)
+            i = cols - 1;
 
         return i;
-    }	
-    
+    }
+
     //Simply makes a coordinate a valid one (i.e within the boundaries of the Board)
-    public int makeValidCoordinateY(int i)
-    {
+    public int makeValidCoordinateY(int i) {
         if (i < 0)
             i = 0;
-        else if (i > cols-1)
-            i = cols-1;
+        else if (i > rows - 1)
+            i = rows - 1;
 
         return i;
-    }	
-    
+    }
+
     //------------------------------------------------------------------//	        
 
     //-------------DATA BASE------------------------//
-    
+
     // to check whether there is a save game or not
-    public boolean checkSave()
-    {
+    public boolean checkSave() {
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
-        
+
         boolean saveExists = false;
 
         try {
-            String dbURL = Game.dbPath; 
-            
-            connection = DriverManager.getConnection(dbURL); 
+            String dbURL = Game.dbPath;
+
+            connection = DriverManager.getConnection(dbURL);
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * FROM GAME_STATE");
-            
-            while(resultSet.next()) 
-            {
+
+            while (resultSet.next()) {
                 saveExists = true;
             }
-            
+
             // cleanup resources, once after processing
             resultSet.close();
             statement.close();
-                       
+
             // and then finally close connection
-            connection.close();            
-            
+            connection.close();
+
             return saveExists;
-        }
-        catch(SQLException sqlex)
-        {
+        } catch (SQLException sqlex) {
             sqlex.printStackTrace();
             return false;
-        }        
+        }
     }
-    
+
     //--------------LOAD SAVED GAME-----------------//
-    
-    
-    public Pair loadSaveGame()
-    {
+
+
+    public Pair loadSaveGame() {
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
 
         try {
-            String dbURL = Game.dbPath; 
-            
-            connection = DriverManager.getConnection(dbURL); 
-            
+            String dbURL = Game.dbPath;
+
+            connection = DriverManager.getConnection(dbURL);
+
             //--------------Load Cells State-------------------//
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * FROM CELL");
 
-            for(int x = 0 ; x < rows ; x++)
-            {
-                for(int y = 0 ; y < cols ; y++)
-                {                                        
+            for (int x = 0; x < rows; x++) {
+                for (int y = 0; y < cols; y++) {
                     resultSet.next();
-                    
+
                     cells[x][y].setContent(resultSet.getString("CONTENT"));
                     cells[x][y].setMine(resultSet.getBoolean("MINE"));
-                    cells[x][y].setSurroundingMines(resultSet.getInt("SURROUNDING_MINES"));                    
+                    cells[x][y].setSurroundingMines(resultSet.getInt("SURROUNDING_MINES"));
                 }
             }
-            
+
             statement.close();
             resultSet.close();
             //----------------------------------------------------//
@@ -247,154 +215,136 @@ public class Board
             resultSet = statement.executeQuery("SELECT * FROM GAME_STATE");
 
             resultSet.next();
-                        
-            Pair p = new Pair(resultSet.getInt("TIMER"),resultSet.getInt("MINES"));
-            
+
+            Pair p = new Pair(resultSet.getInt("TIMER"), resultSet.getInt("MINES"));
+
             //After loading, delete the saved game
             deleteSavedGame();
-            
+
             // cleanup resources, once after processing
             resultSet.close();
             statement.close();
-                       
+
             // and then finally close connection
             connection.close();
 
             return p;
-        }
-        catch(SQLException sqlex)
-        {
+        } catch (SQLException sqlex) {
             sqlex.printStackTrace();
             return null;
-        }                
-    }
-    
-    
-    //------------------------------------------------------------------------//
-    public void deleteSavedGame()
-    {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        
-        try {
-            String dbURL = Game.dbPath; 
-            
-            connection = DriverManager.getConnection(dbURL); 
-
-            
-            //----------EMPTY GAME_STATE TABLE------//
-            String template = "DELETE FROM GAME_STATE"; 
-            statement = connection.prepareStatement(template);
-            statement.executeUpdate();
-            
-            //----------EMPTY CELL TABLE------//
-            template = "DELETE FROM CELL"; 
-            statement = connection.prepareStatement(template);
-            statement.executeUpdate();
-            
-            statement.close();
-            
-            // and then finally close connection
-            connection.close();            
         }
-        catch(SQLException sqlex)
-        {
-            sqlex.printStackTrace();
-        }                
     }
-    
-           
-    //--------------SAVE GAME IN DATABASE-----------//
-    public void saveGame(int timer, int mines)
-    {
+
+
+    //------------------------------------------------------------------------//
+    public void deleteSavedGame() {
         Connection connection = null;
         PreparedStatement statement = null;
-        
-        try {
-            String dbURL = Game.dbPath; 
-            
-            connection = DriverManager.getConnection(dbURL); 
 
-            
+        try {
+            String dbURL = Game.dbPath;
+
+            connection = DriverManager.getConnection(dbURL);
+
+
+            //----------EMPTY GAME_STATE TABLE------//
+            String template = "DELETE FROM GAME_STATE";
+            statement = connection.prepareStatement(template);
+            statement.executeUpdate();
+
+            //----------EMPTY CELL TABLE------//
+            template = "DELETE FROM CELL";
+            statement = connection.prepareStatement(template);
+            statement.executeUpdate();
+
+            statement.close();
+
+            // and then finally close connection
+            connection.close();
+        } catch (SQLException sqlex) {
+            sqlex.printStackTrace();
+        }
+    }
+
+
+    //--------------SAVE GAME IN DATABASE-----------//
+    public void saveGame(int timer, int mines) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            String dbURL = Game.dbPath;
+
+            connection = DriverManager.getConnection(dbURL);
+
+
             //--------------INSERT DATA INTO CELL TABLE-----------//            
             String template = "INSERT INTO CELL (CONTENT, MINE, SURROUNDING_MINES) values (?,?,?)";
             statement = connection.prepareStatement(template);
 
-            for(int x = 0 ; x < rows ; x++)
-            {
-                for(int y = 0 ; y < cols ; y++)
-                {
+            for (int x = 0; x < rows; x++) {
+                for (int y = 0; y < cols; y++) {
                     statement.setString(1, cells[x][y].getContent());
                     statement.setBoolean(2, cells[x][y].getMine());
-                    statement.setInt(3, (int)cells[x][y].getSurroundingMines());                    
+                    statement.setInt(3, (int) cells[x][y].getSurroundingMines());
 
                     statement.executeUpdate();
                 }
             }
             //--------------------------------------------------//
 
-            
+
             //--------------------SAVE GAME STATE----------------------//
             template = "INSERT INTO GAME_STATE (TIMER,MINES) values (?,?)";
             statement = connection.prepareStatement(template);
-            
+
             statement.setInt(1, timer);
             statement.setInt(2, mines);
 
             statement.executeUpdate();
-            
+
             //---------------------------------------------------------//
-            
+
             statement.close();
-            
+
             // and then finally close connection
-            connection.close();            
-        }
-        catch(SQLException sqlex)
-        {
+            connection.close();
+        } catch (SQLException sqlex) {
             sqlex.printStackTrace();
         }
-        
+
     }
 
 
     //--------------------------------------------//
     //---------GETTERS AND SETTERS-------------//
-    public void setNumberOfMines(int numberOfMines)
-    {
+    public void setNumberOfMines(int numberOfMines) {
         this.numberOfMines = numberOfMines;
     }
 
-    public int getNumberOfMines()
-    {
+    public int getNumberOfMines() {
         return numberOfMines;
     }
 
-    public Cell[][] getCells()
-    {
+    public Cell[][] getCells() {
         return cells;
     }
-    
-    public int getRows()
-    {
+
+    public int getRows() {
         return rows;
     }
-    
-    public int getCols()
-    {
+
+    public int getCols() {
         return cols;
     }
     //-----------------------------------------//
 
-    public void resetBoard()
-    {
-        for(int x = 0 ; x < rows ; x++)
-        {
-            for(int y = 0 ; y < cols ; y++)
-            {
-                cells[x][y].setContent("");                        
+    public void resetBoard() {
+        for (int x = 0; x < cols; x++) {
+            for (int y = 0; y < rows; y++) {
+                cells[x][y].setContent("");
             }
         }
     }
-    
+
 }
